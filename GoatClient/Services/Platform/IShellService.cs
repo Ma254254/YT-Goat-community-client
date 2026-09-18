29b1edfe6e0ff0b58acd0d0d4b83d4be292375aa
Ma254 <marlon.albrecht.leu@gmail.com>
2026-09-18 20:20:07 +0200

    Delete GoatClient directory

--- GoatClient/Services/Platform/IShellService.cs
+++
@@ -1,7 +0,0 @@
-namespace GoatClient.Services.Platform;
-
-public interface IShellService
-{
-    /// <summary>Opens a folder in Windows Explorer. Returns false if it does not exist.</summary>
-    bool OpenFolder(string path);
-}
